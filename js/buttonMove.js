932ba3101ec9a482d1e2d7a087fff3f4d81be819
GabrielPelland <gabrielpelland@hotmail.com>
2021-11-14 18:04:05 -0500

    Cawliss de bon ux fo button

--- js/buttonMove.js
+++ js/buttonMove.js
@@ -1,6 +1,26 @@
+var e = e || window.event;
+var isFollowing = false;
+
 $(document).ready(function(){
     $( "a.mouseOver" ).mouseover(function() {
-        console.log("Mouse over");
-        $("a.mouseOver").addClass("absolute");
+        $("#button-move").addClass("absolute");
+        isFollowing = true;
     });
+});
+
+jQuery(function($) {
+    mousePos = { x : 0, y : 0 }; 
+    $(document).mousemove(function(event) {
+        mousePos.x = event.pageX - 70;
+        mousePos.y = event.pageY;
+    });
+});
+
+$(document).on('mousemove', function(e){
+    if(isFollowing == true) {
+        $('#button-move').css({
+            left:  mousePos.x,
+            top:   mousePos.y
+        });
+    }
 });
