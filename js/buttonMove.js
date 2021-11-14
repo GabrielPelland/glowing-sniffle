ef0c2372b57d86c9cc1a3537cfdcc7b2c38c0c9e
GabrielPelland <gabrielpelland@hotmail.com>
2021-11-14 17:24:25 -0500

    Less trash code shit

--- js/buttonMove.js
+++ js/buttonMove.js
@@ -1,4 +1,6 @@
-$( "a.mouseOver" )
-  .mouseover(function() {
-    console.log("Mouse over");
-  })
+$(document).ready(function(){
+    $( "a.mouseOver" ).mouseover(function() {
+        console.log("Mouse over");
+        $("a.mouseOver").addClass("absolute");
+    });
+});
