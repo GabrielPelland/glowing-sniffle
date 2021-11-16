e11dc57ae8c33b9c284cd7f9b294df2b0871ca5b
GabrielPelland <gabrielpelland@hotmail.com>
2021-11-15 22:51:00 -0500

    popo

--- js/cookie.js
+++ js/cookie.js
@@ -1,8 +1,30 @@
-$(document).ready(function(){
-    setTimeout(() => {
-        $('#cookie').slideDown(20000);    
-    }, 15000);
+var mouseClick = false;
+
+$(document).ready(function () {
+  setTimeout(() => {
+    $("#cookie").slideDown(20000);
+  }, 15000);
 });
 
+$(document).ready(function () {
+  $("#cookie-off").mousedown(function () {
+    console.log("MouseDown");
+    mouseClick = true;
+    setTimeout(() => {
+      if (mouseClick == true) {
+        console.log("Close");
+        closeCookie();
+      };
+    }, 5000);
+  });
+});
 
+$(document).ready(function () {
+  $("#cookie-off").mouseleave(function () {
+      mouseClick = false;
+  });
+});
 
+function closeCookie(){
+    $("#cookie").slideUp(20000);
+}
